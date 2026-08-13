eef8928f67be9574372e40e6f3fc17a8eed6ba07
Korshi <kerriganfv@gmail.com>
2026-08-13 21:52:09 +0300

    Target Android API 36

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "dev.healthbridge"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "dev.healthbridge"
         minSdk = 28
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "0.1.0"
     }
